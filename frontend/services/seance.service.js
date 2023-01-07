79bf135c860d89a9d55998910864e493dbdea17f
deathsh0ot <ala-hmz@outlook.com>
2023-01-07 02:23:22 +0100

    fix: fixed minor error

--- frontend/services/seance.service.js
+++ frontend/services/seance.service.js
@@ -1,8 +1,8 @@
-iimport axios from "axios";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = "http://192.168.70.210:8080/api/coach/seance/";
-const API_URL_Player = "http://192.168.70.210:8080/api/joueur/seance/";
+const API_URL = 'http://192.168.70.210:8080/api/coach/seance/';
+const API_URL_Player = 'http://192.168.70.210:8080/api/joueur/seance/';
 
 function delay(ms) {
   return new Promise((resolve) => {
@@ -11,24 +11,24 @@ function delay(ms) {
 }
 
 const fetchSeance = async () => {
-  const token = await AsyncStorage.getItem("@user");
+  const token = await AsyncStorage.getItem('@user');
   const result = await axios.get(`${API_URL}${token}`);
   return result.data;
 };
 
 const postSeance = async (seance) => {
-  AsyncStorage.getItem("@user").then(async (token) => {
-   const result = await axios.post(API_URL + token, seance);
+  AsyncStorage.getItem('@user').then(async (token) => {
+    const result = await axios.post(API_URL + token, seance);
     return result.data;
   });
 };
 
 const fetchSeances = async (searchValue) => {
-  const token = await AsyncStorage.getItem("@user");
+  const token = await AsyncStorage.getItem('@user');
   const results = await axios.get(`${API_URL}${token}`);
   return results.data.filter((seance) => seance.periode.includes(searchValue));
 };
- const fetchSeanceToday = async () => {
+const fetchSeanceToday = async () => {
   await delay(2000);
   const result = await axios.get(`${API_URL}${token}`);
   const today = new Date();
@@ -40,11 +40,7 @@ const fetchSeances = async (searchValue) => {
   return result.data.filter((seance) => seance.date.includes(seanceDate));
 };
 
- const fetchSeanceByFields = async (
-  seanceNom,
-  seanceDate,
-  seancePeriode
-) => {
+const fetchSeanceByFields = async (seanceNom, seanceDate, seancePeriode) => {
   await delay(2000);
   const result = await axios.get(`${API_URL_Player}${token}`);
   return result.data.filter(
@@ -60,7 +56,6 @@ const SeanceService = {
   fetchSeanceByFields,
   fetchSeanceToday,
   fetchSeances,
-}
+};
 
 export default SeanceService;
-
