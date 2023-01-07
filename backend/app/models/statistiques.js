d064219cdc8deddbc35cc6526b6afc5791eaf57c
deathsh0ot <ala-hmz@outlook.com>
2023-01-07 04:20:58 +0100

    fix: stat crud is now functional

--- backend/app/models/statistiques.js
+++ backend/app/models/statistiques.js
@@ -1,44 +1,46 @@
 const mongoose = require('mongoose');
 
-const statistiquesSchema = new mongoose.Schema({
+const statistiquesSchema = new mongoose.Schema(
+  {
     nom: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     type: {
-        type: String,
-        enum: ['Compteur', 'Timer'],
-        required: true
+      type: String,
+      // enum: ['Compteur', 'Timer'],
+      required: true,
     },
     unite: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     objectif: {
-        type: String,
-        enum: ['Max', 'Min'],
-        required: true
+      type: String,
+      // enum: ['Max', 'Min'],
+      required: true,
     },
     lien: {
-        type: String,
-        // required: true
+      type: String,
+      // required: true
     },
     visible: {
-        type: Boolean,
-        required: true,
+      type: Boolean,
+      // default: true,
+      //   required: true,
     },
     createdBy: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    }
-}, {
-    timestamps:true
-}
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
-
-module.exports = mongoose.model("Statistiques", statistiquesSchema);
+module.exports = mongoose.model('Statistiques', statistiquesSchema);
